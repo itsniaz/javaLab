096c25c51ecec8a278a1605770a48ff57bf79d66
trialnly <trialnly@users.noreply.github.com>
2017-02-20 16:08:23 +0600

    Add files via upload

---
+++ Title.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.io.*;
+public class Title
+{
+		private String name;
+		
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+		
+		public void addT()
+		{
+			name = name.trim();
+			if(name.startsWith("Amy")==true)
+			{
+				System.out.println("Ms. "+name);
+			}
+			else if(name.startsWith("Buffy")==true)
+			{
+				System.out.println("Ms. "+name);
+			}
+			else if(name.startsWith("Cathy")==true)
+			{
+				System.out.println("Ms. "+name);
+			}
+			else if(name.startsWith("Elroy")==true)
+			{
+				System.out.println("Mr. "+name);
+			}
+			else if(name.startsWith("Fred")==true)
+			{
+				System.out.println("Mr. "+name);
+			}
+			else if(name.startsWith("Graham")==true)
+			{
+				System.out.println("Mr. "+name);
+			}
+			else
+			{
+				System.out.println("Mr. "+name);
+			}
+		}
+		
+		public static void main (String[] args)
+		{
+			Scanner inp = new Scanner(System.in);
+			Title t = new Title();
+			System.out.printf("Enter a name : ");
+			String name = inp.nextLine();
+			t.setName(name);
+			t.addT();
+			
+		}
+}
