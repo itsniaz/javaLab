f78c21167496beb9a4ddaaa2d3c74808a3227f16
itzniaz <trialnly@gmail.com>
2017-03-03 23:35:39 +0600

    Assignment

---
+++ Assignment/Bank/main.java
@@ -0,0 +1,28 @@
+public class main{
+	public static void main(String a[]){
+		Account sen=new Account("SA-RA-1","Rahim",1500);
+		Account rec=new Account("SA-KA-2","Karim",1200);
+		
+		Account sen2=new Account("SA-RA-1","Raju",2000);
+		Account rec2=new Account("SA-MI-1","Mina",2100);
+		
+		Bank b=new Bank();
+
+		System.out.println("\n-------------Before Trasaction-----------\n");
+		sen.showInfo();
+		rec.showInfo();
+		sen2.showInfo();
+		rec2.showInfo();
+
+
+		b.addTransaction(sen,rec,130);
+		b.addTransaction(sen2,rec2,5000);
+		System.out.println("\n-------------After Trasaction-----------\n");
+		sen.showInfo();
+		rec.showInfo();
+		sen2.showInfo();
+		rec2.showInfo();
+		
+		b.showAllTransaction();
+	}
+}
