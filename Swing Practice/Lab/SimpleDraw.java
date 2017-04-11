ea86ad6967d1d586bba5e199bca3f3250556aa2d
Niaz <you@example.com>
2017-04-11 15:42:23 +0600

    .

--- Swing Practice/Lab/SimpleDraw.java
+++ Swing Practice/Lab/SimpleDraw.java
@@ -1,19 +1,26 @@
+import java.util.ArrayList;
+import javax.swing.*;
 import java.awt.*;
-class MyFrame extends Frame{
+class MyFrame extends JFrame{
 	  DimensionInterface r[];
+	  ArrayList list;
 	public MyFrame(){
 		super("Java First Window");
 		setSize(600,400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());
 		
 	}
-	public void setShapes(DimensionInterface recs[]){
-		this.r=recs;
+	public void setShapes(ArrayList list){
+		this.list = list;
 	}
 	public void paint(Graphics g){
-		  for(int i = 0; i<r.length;i++)
+		  for(int i = 0; i<list.size();i++)
 		  {
-			  g.drawOval(r[i].getX(),r[i].getY(),r[i].getWidth(),r[i].getHeight());
+			  Rectangle r = new Rectangle(0,0,0,0);
+			 //System.out.println(list.get(i));
+			 
+		 g.drawRect(r.getX(),r.getY(),r.getWidth(),r.getHeight());
 			  //System.out.println(r[i].toString());
 		  }
 	}
@@ -24,13 +31,16 @@ public class SimpleDraw{
         mf.setVisible(true);
 		
 		DimensionInterface [] obj = new DimensionInterface[5];
-
-		obj[0] = new Rectangle(0,0,200,100);
-		obj[1] = new Circle(50,50,90,100);
-		obj[2] = new Circle(40,50,50,100);
-		obj[3] = new Rectangle(50,50,50,100);
-		obj[4] = new Rectangle(50,50,50,100);
+ 
+		ArrayList <DimensionInterface> list =  new ArrayList <DimensionInterface>();
+		list.add(new Rectangle(0,0,200,100));
+	    list.add(new Rectangle(50,50,90,100));
+		list.add(new Rectangle(40,50,50,100));
+		list.add(new Rectangle(50,50,50,100));
+		list.add(new Rectangle(50,50,50,100));
 		
-		mf.setShapes(obj);
+		
+		
+		mf.setShapes(list);
     }
 }
